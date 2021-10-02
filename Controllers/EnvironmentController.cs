613e5e52e7b945957216cf115649ba2d765b1220
mweaver-inmusic <80358035+mweaver-inmusic@users.noreply.github.com>
2021-10-02 12:11:08 -0400

    fully built connection string available to read

--- Controllers/EnvironmentController.cs
+++ Controllers/EnvironmentController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
@@ -14,13 +15,24 @@ namespace ContosoPizza.Controllers
             var dburl = Environment.GetEnvironmentVariable("DATABASE_URL");
             var uri = new Uri(dburl);
             var userInfo = uri.UserInfo.Split(":");
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = uri.Host,
+                Port = uri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = uri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            };
             return new DbUrl { 
                 FullString = dburl,
                 Host = uri.Host,
                 Port = uri.Port.ToString(),
                 Username = userInfo[0],
                 Password = userInfo[1],
-                Database = uri.LocalPath.TrimStart('/')
+                Database = uri.LocalPath.TrimStart('/'),
+                ConnectionString = builder.ToString()
             };
         }
     }
@@ -33,5 +45,6 @@ namespace ContosoPizza.Controllers
         public string Username { get; set; }
         public string Password { get; set; }
         public string Database { get; set; }
+        public string ConnectionString { get; set; }
     }
 }
