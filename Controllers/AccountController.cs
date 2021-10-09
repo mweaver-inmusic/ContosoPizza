20bf27dd46dffa8291a5ec17f3abe43b03321007
mweaver-inmusic <80358035+mweaver-inmusic@users.noreply.github.com>
2021-10-08 21:00:32 -0400

    get all users and signout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoPizza.Controllers
 {
@@ -17,22 +19,36 @@ namespace ContosoPizza.Controllers
             _signInManager = signInManager;
         }
 
+        [HttpGet("users")]
+        public async Task<List<IdentityUser>> GetAsync()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+
         [HttpPost("login")]
         public async Task<IActionResult> PostAsync(Login login)
         {
             var user = await _userManager.FindByEmailAsync(login.Email);
             var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
-            if(!result.Succeeded) return BadRequest();
+            if(!result.Succeeded) return BadRequest("incorrect password");
             return Ok(login);
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> PostAsync(Register register)
         {
-            var user = new IdentityUser { Email = register.Email, UserName = register.Username };
+            var user = await _userManager.FindByEmailAsync(register.Email);
+            if(user != null) return BadRequest("User already in system");
+            user = new IdentityUser { Email = register.Email, UserName = register.Email };
             var result = await _userManager.CreateAsync(user, register.Password);
             if(!result.Succeeded) return BadRequest();
             return Ok(register);
+        }
+
+        [HttpGet("signout")]
+        public async Task Signout()
+        {
+            await _signInManager.SignOutAsync();
         }
     }
 
@@ -44,7 +60,7 @@ namespace ContosoPizza.Controllers
 
     public class Register
     {
-        public string Username { get; set; }
+        // public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
